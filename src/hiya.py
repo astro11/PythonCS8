47808af283a527f61fcb25b15d90dcd0c460d477
astro11 <aquila.strother@asu.edu>
2018-06-18 18:45:07 -0400

    testing

--- src/hiya.py
+++ src/hiya.py
@@ -1,3 +1,18 @@
 # hello python
 
 print('hello world!')
+firstName = 'aquila'
+lastName = 'strother'
+# print(firstName.capitalize())
+# print(lastName.capitalize())
+print('Hiya' + ' ' + firstName.capitalize() + ' ' + lastName.capitalize())
+
+# One statement per line
+age = int(input("Please enter your age: "))
+print(age)
+if age < 18:
+    age = 18
+    print('You are not old enough to skydive buddy')
+else:
+        print('You can now plunge from the sky!')
+        
